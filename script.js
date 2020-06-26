9edf029cae8e778d0d6eb1b232e4ca3cf3a51649
Randalla <the.randall.lim@gmail.com>
2020-06-26 11:33:44 +0800

    base

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.querySelector('red').addEventListener('click', function(event){
+//     // let click = event.target.value;
+//     // function alert(){
+//     //     if click 
+//         alert("red");
+//     }
+//     // let output = inputHappened(clicked);
+//--------------------------------------------------------------------------------------------
+//   });
+
+// //     let display = function( data ){
+// //     let output = document.querySelector('#output');
+// //     output.innerText = data;
+// //   }
+
+
+// // function clicked(event) {
+// //     let clickk = document.querySelector('.'+ event.target.className)
+
